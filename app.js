9d71eb2a287113feaf07349d29327bd74cf5c121
Anjali0708-gt <anj13062004@gmail.com>
2026-02-15 19:04:52 +0530

    Updated app.js configuration

--- app.js
+++ app.js
@@ -22,5 +22,5 @@ app.use("/api/auth", router);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(` Server running on port ${PORT}`);
 });
