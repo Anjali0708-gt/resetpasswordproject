92981c39df6f416b77d1a1832d0558e94ce4d65d
Anjali0708-gt <anj13062004@gmail.com>
2026-03-24 18:52:14 +0530

    Fix CORS for frontend

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import router from "./route/authroute.js";
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+app.use(cors({ origin: 'http://localhost:5173' }));
 
 // Middleware
 app.use(express.json());
