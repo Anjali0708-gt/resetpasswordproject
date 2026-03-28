e557f89ff468041dead79c7eef9414f213a583db
Anjali0708-gt <anj13062004@gmail.com>
2026-03-28 20:46:17 +0530

    reset update

--- controller/authcontroller.js
+++ controller/authcontroller.js
@@ -69,7 +69,8 @@ export const  login= async(req,res)=>
           const user= await User.findOne({email});
          
           
- if (!user) return res.status(404).json({ msg: "user not registered" })
+ if (!user) 
+  {return res.status(404).json({ msg: "user not registered" })}
         //   token generation
           const token= jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"1h" });
           // const link = `http://localhost:5173/reset-password/${token}`;
